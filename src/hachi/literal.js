f4b69e9d111f6a81a30e265fd9489c0ebad0378a
hachi <eojin312@naver.com>
2020-03-09 14:15:47 +0900

    객체생성

---
+++ src/hachi/literal.js
@@ -0,0 +1,13 @@
+/**
+ * 리터럴 객체 생성
+ */
+
+
+var foo = {
+    name : 'foo',
+    age : 30,
+    gender : 'male'
+};
+
+document.write(foo);
+document.write(typeof foo);
---
+++ src/hachi/object.js
@@ -0,0 +1,17 @@
+/**
+ * 객체 생성 javascript
+ */
+
+//object() 를 이용해서 foo 빈 객체 생성
+
+var foo = new Object();
+
+//foo 객체 프로퍼티 생성
+foo.name = 'foo';
+foo.age = 30;
+foo.gender = 'male';
+
+console.log(typeof  foo); // (출력값) object
+console.log(foo);
+
+document.write(foo);
