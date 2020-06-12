63528a35f9f008cb2ef450984fac5fa8a643353d
hachi <eojin312@naver.com>
2020-06-12 18:15:28 +0900

    js 연습 - 함수 객체 - 생성자 함수 - 함수를 다른 함수 인자로 넘김

--- src/hachi/object.js
+++ src/hachi/object.js
@@ -18,3 +18,36 @@ foo.gender = 'male';
 
 foo.major = 'electronics engineering';
 foo['full-Name'] = 'foo bar';
+
+/**
+ * 생성자 함수
+ * @type {Function}
+ */
+var add = new Function('x', 'y', 'return x + y');
+console.log(add(3, 4));
+
+/**
+ * 함수 = 객체 라는 것을 증명하기위한 메소드
+ * @param x
+ * @param y
+ * @returns {*}
+ */
+function sum(x, y) {
+    return x + y;
+}
+sum.result = add(3, 2);
+sum.status = 'OK';
+
+console.log(sum.result);
+console.log(sum.status);
+
+/**
+ * 함수를 다른 함수 인자로 넘겨버린 메소드
+ * @param func
+ */
+var foo = function (func) {
+    func();
+}
+foo(function () {
+    console.log('Function can be used as the argument');
+});
