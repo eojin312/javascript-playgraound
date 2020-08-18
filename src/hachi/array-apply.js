ea44633e4f3c28ff1e5e9c25c978e0a423e66c91
hachi <eojin312@naver.com>
2020-08-18 21:42:50 +0900

    vue.js 연습

--- src/hachi/array-apply.js
+++ src/hachi/array-apply.js
@@ -3,14 +3,13 @@
  * @type {string[]}
  */
 
-var arr = ['bar'];
+var arr = ['bar', 'eojin', 'hachi'];
 var obj =  {name : 'foo', length : 1}
 
 
+// splice() 는 원소 자체를 삭제 delete() 는 원소를 undefined 로만 설정한다.
+arr.splice(0, 1); // 0 번째 원소를 기준으로 1개의 원소를 삭제한다.
 arr.push('baz');
 console.log(arr);
-
 Array.prototype.push.apply(obj, ['baz']);
-
-
 console.log(abj);
